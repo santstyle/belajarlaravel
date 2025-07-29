81bde2328f9bfcf8b9770c0faab57e91fdc71b8a
santstyle <adjixsantosoputra@gmail.com>
2025-07-29 19:13:12 +0700

    Comment web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
+
+// Route Blog Start
 Route::get('/posts', function () {
     $posts = Post::all();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
@@ -16,6 +18,7 @@ Route::get('posts/{post:slug}', function(Post $post) {
 
         return view('post', ['title'=> 'Single Post', 'post' => $post]);
 });
+// Route Blog End
 
 Route::get('/about', function () {
     return view('about', ['title'=> 'About']);
