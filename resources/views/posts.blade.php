e8376f1db9f6fdae184aaf280cfb2b762fa6fbd3
santstyle <adjixsantosoputra@gmail.com>
2025-07-29 10:26:50 +0700

    halaman blog

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,14 @@
 <x-layout :title="$title">
 
-    <article class="py-8 max-w-screen-md border-b border-grey-300">
-        <h2 class="mb-1 text-3-xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
-        <div class="text-base text-gray-500">
-            <a href="">SantStyle</a> | 1 Januari 2025
-        </div>
-        \
-
-        <p class="my-4 font-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet iure alias laborum
-            sint, necessitatibus
-            asperiores eligendi, hic totam sapiente voluptate assumenda. Odit deleniti voluptatem excepturi assumenda
-            repellat quae dolor recusandae.</p>
-        <a href="3" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-    </article>
-    <article class="py-8 max-w-screen-md border-b border-grey-300">
-        <h2 class="mb-1 text-3-xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
-        <div class="text-base text-gray-500">
-            <a href="">SantStyle</a> | 2 Januari 2025
-        </div>
-        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse perspiciatis est
-            voluptatibus suscipit, nisi expedita assumenda corrupti quaerat asperiores, tenetur, rem qui. Reprehenderit
-            eligendi, voluptatum culpa eius quibusdam corrupti. Impedit!</p>
-        <a href="3" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-    </article>
+    @foreach ($posts as $post)
+        <article class="py-8 max-w-screen-md border-b border-grey-300">
+            <h2 class="mb-1 text-3-xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+            <div class="text-base text-gray-500">
+                <a href="">{{ $post['author'] }}</a> | 1 Januari 2025
+            </div>
+            <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
+            <a href="3" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+        </article>
+    @endforeach
 
 </x-layout>
