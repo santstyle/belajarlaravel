d191329610563f522ccc74268b887d60cfc920a0
santstyle <adjixsantosoputra@gmail.com>
2025-07-29 19:07:34 +0700

    Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
-Route::get('posts/{id}', function($id) {
+Route::get('posts/{post:slug}', function(Post $post) {
 
-        $post = Post::find($id);
         return view('post', ['title'=> 'Single Post', 'post' => $post]);
 });
 
