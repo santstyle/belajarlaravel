c050ad90384d6c80ca939d64bc6b96532d91c67a
santstyle <adjixsantosoputra@gmail.com>
2025-08-13 20:06:25 +0700

    deskrips juduli

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,4 +7,7 @@
                 {{ $i }}</div>
         @endfor
     </div>
+    <div>
+        <h1 class="text-xl mt-4 font-medium">This is a First Project of My Laravel Framework</h1>
+    </div>
 </x-layout>
