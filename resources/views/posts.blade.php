b6283094b6cbeba658d6eb10d0aca5f375ffc3dd
santstyle <adjixsantosoputra@gmail.com>
2025-08-17 12:22:35 +0700

    Post Category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,9 @@
                 <h2 class="mb-1 text-3-xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-base text-gray-500">
-                <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> | 1 Januari 2025
+                By <a href="/authors/{{ $post->author->username }}" class="text-gray-900 hover:underline">{{ $post->author->name }}</a>
+                in <a href="/categories/{{ $post->category->slug }}" class="text-gray-900 hover:underline">{{ $post->category->name }}</a>
+                | 1 Januari 2025
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 
@@ -22,6 +23,10 @@ Route::get('posts/{post:slug}', function(Post $post) {
 
 Route::get('/authors/{user:username}', function (User $user) {
     return view('posts', ['title' => count($user->posts) .  ' Article by. ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', ['title' => 'Category: . ' . $category->name, 'posts' => $category->posts]);
 });
 
 // Route Blog End
