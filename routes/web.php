56954fcfee1cc152854e020ae131859d18400e5f
santstyle <adjixsantosoputra@gmail.com>
2025-08-22 09:27:45 +0700

    N+1 Problem

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ Route::get('/', function () {
 
 // Route Blog Start
 Route::get('/posts', function () {
-    $posts = Post::all();
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
@@ -22,10 +23,15 @@ Route::get('posts/{post:slug}', function(Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
-    return view('posts', ['title' => count($user->posts) .  ' Article by. ' . $user->name, 'posts' => $user->posts]);
+    // $posts = $user->posts->load('category','author');
+
+    return view('posts', ['title' => count ($user->posts) .  ' Article by. ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+
+    // $posts = $category->posts->load('category', 'author');
+    
     return view('posts', ['title' => 'Category: . ' . $category->name, 'posts' => $category->posts]);
 });
 
