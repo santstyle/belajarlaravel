715b77118301a7779e63263e473e51368bb79ae9
santstyle <adjixsantosoputra@gmail.com>
2025-08-22 20:06:50 +0700

    Simple Post Searching

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,27 @@
 <x-layout :title="$title">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div class="py-4 px-4 mx-auto max-w-screen-xl lg:px-6">
+
+
+        <form class="mb-8 max-w-md mx-auto">
+            <label for="default-search"
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input type="search" id="default-search"
+                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search Post title..." required autofocus />
+                <button type="submit"
+                    class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+        </form>
+
+
         <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3 ">
             @foreach ($posts as $post)
                 <article
@@ -17,14 +39,16 @@
                             href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a></h2>
                     <p class="mb-5 font-light text-gray-500 dark:text-gray-400">{{ Str::limit($post->body) }}</p>
                     <div class="flex justify-between items-center">
-                        <div class="flex items-center space-x-4">
-                            <img class="w-7 h-7 rounded-full"
-                                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
-                                alt="{{ $post->author->name }}" />
-                            <span class="font-medium text-xs dark:text-white">
-                                {{ $post->author->name }}
-                            </span>
-                        </div>
+                        <a href="/authors/{{ $post->author->username }}">
+                            <div class="flex items-center space-x-4">
+                                <img class="w-7 h-7 rounded-full"
+                                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                                    alt="{{ $post->author->name }}" />
+                                <span class="font-medium text-xs dark:text-white">
+                                    {{ $post->author->name }}
+                                </span>
+                            </div>
+                        </a>
                         <a href="/posts/{{ $post['slug'] }}"
                             class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
                             Read more
--- routes/web.php
+++ routes/web.php
@@ -13,32 +13,32 @@ Route::get('/', function () {
 // Route Blog Start
 Route::get('/posts', function () {
     // $posts = Post::with(['author', 'category'])->latest()->get();
-    $posts = Post::latest()->get();
+    $posts = Post::where('author_id', 1)->get();
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
-Route::get('posts/{post:slug}', function(Post $post) {
+Route::get('posts/{post:slug}', function (Post $post) {
 
-        return view('post', ['title'=> 'Single Post', 'post' => $post]);
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
     // $posts = $user->posts->load('category','author');
 
-    return view('posts', ['title' => count ($user->posts) .  ' Article by. ' . $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) .  ' Article by. ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
 
     // $posts = $category->posts->load('category', 'author');
-    
+
     return view('posts', ['title' => 'Category: . ' . $category->name, 'posts' => $category->posts]);
 });
 
 // Route Blog End
 
 Route::get('/about', function () {
-    return view('about', ['title'=> 'About']);
+    return view('about', ['title' => 'About']);
 });
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Us']);
