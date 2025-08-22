3a618278744fb538b1fb532aba89094a2843956b
santstyle <adjixsantosoputra@gmail.com>
2025-08-22 19:36:50 +0700

    Update UI Halaman Single Post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -19,33 +19,35 @@ URL: https://flowbite.com/docs/components/typography/
 
         <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
             <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
-                <a href="/posts" class="font-medium text-blue-500 hover:underline">
-                    &laquo; Back to all posts.</a>
                 <article
                     class="mx-auto w-full max-w-4xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-                    <header class="mb-4 lg:mb-6 not-format">
+                    <a href="/posts" class="font-medium text-xs text-blue-500 hover:underline">
+                        &laquo; Back to all posts.</a>
+                    <header class="my-4 lg:mb-6 not-format">
                         <address class="flex items-center mb-6 not-italic">
                             <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                                 <img class="mr-4 w-16 h-16 rounded-full"
-                                    src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
+                                    src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                                    alt="{{ $post->author->name }}">
                                 <div>
-                                    <a href="#" rel="author"
-                                        class="text-xl font-bold text-gray-900 dark:text-white">Jese Leos</a>
-                                    <p class="text-base text-gray-500 dark:text-gray-400">Graphic Designer, educator &
-                                        CEO Flowbite</p>
+                                    <a href="/authors/{{ $post->author->username }}" rel="author"
+                                        class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
+                                    <a href="/authors/{{ $post->category->slug }}" class="block">
+                                        <span
+                                            class="{{ $post->category->color }} text-grey-600 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                            {{ $post->category->name }}
+                                        </span>
+                                    </a>
                                     <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate
-                                            datetime="2022-02-08" title="February 8th, 2022">Feb. 8, 2022</time></p>
+                                            datetime="2022-02-08"
+                                            title="February 8th, 2022">{{ $post->created_at->diffForHumans() }}
                                 </div>
-                            </div>
                         </address>
                         <h1
                             class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
-                            Best practices for successful prototypes</h1>
+                            {{ $post['title'] }}</h1>
                     </header>
-                    <p>Before going digital, you might benefit from scribbling down some ideas in a sketchbook. This
-                        way,
-                        you can think things through before committing to an actual design project.</p>
+                    <p>{{ $post['body'] }}</p>
                 </article>
             </div>
-        </main>
     </x-layout>
