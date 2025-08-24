6b8bd20d95be68c178b1457d47b16fd049034719
santstyle <adjixsantosoputra@gmail.com>
2025-08-24 20:49:41 +0700

    If not found

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -30,7 +30,7 @@
         {{ $posts->links() }}
 
         <div class=" my-4 grid gap-8 md:grid-cols-2 lg:grid-cols-3 ">
-            @foreach ($posts as $post)
+            @forelse ($posts as $post)
                 <article
                     class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
@@ -68,7 +68,12 @@
                         </a>
                     </div>
                 </article>
-            @endforeach
+            @empty
+                <div>
+                    <p class="font-semibold text-xl my-4">Articlle not found</p>
+                    <a href="/posts" class="block text-blue-500 hover:underline">&laquo; Back to all posts.</a>
+                </div>
+            @endforelse
         </div>
         {{ $posts->links() }}
     </div>
