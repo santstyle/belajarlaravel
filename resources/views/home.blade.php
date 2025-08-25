34e02a8602d98555027c3b0f2fa00e74354a4606
santstyle <adjixsantosoputra@gmail.com>
2025-08-25 22:27:47 +0700

    Hide h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,6 @@
         @endfor
     </div>
     <div>
-        <h1 class="text-xl mt-4 font-medium">This is a First Project of My Laravel Framework</h1>
+        {{-- <h1 class="text-xl mt-4 font-medium">This is a First Project of My Laravel Framework</h1> --}}
     </div>
 </x-layout>
